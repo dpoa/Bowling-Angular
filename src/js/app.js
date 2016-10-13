b91b1c88887a835e86ba22263c77ae8a7b7df829
Denis Da Silva <Denis@Deniss-MacBook-Pro.local>
2016-10-13 15:45:05 -0300

    Game design and app classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,56 @@
-class MainController {
 
-    constructor($scope) {
-        this.$scope = $scope;    
+(() => {
+    class Player {
+        constructor(name) {
+            this.name = name;
+            this.rolls = [];
+        }
     }
 
-    start () {
-        alert('Start');
-    }
-}
+    class Game {
+        constructor() {
+            this.players = [];
+        }
 
-angular
-    .module('app', [])
-    .controller('MainController', MainController);
+        join (playerName) {
+            var player = new Player(playerName);
+            this.players.push(player);
+        }
+    }
+
+    class GameService {
+
+        constructor() {
+            this.name = '';
+        }
+
+        add(name) {
+
+        }
+    }
+
+    class MainController {
+
+        constructor($scope, GameService) {
+            this.$scope = $scope;
+            this.gameService = GameService;
+        }
+
+        app () {
+            alert('Add');
+        }
+
+        clear () {
+            alert('Add');
+        }
+
+        start () {
+            alert('Start');
+        }
+    }
+
+    angular
+        .module('app', [])
+        .service('GameService', GameService)
+        .controller('MainController', MainController);
+ })();
